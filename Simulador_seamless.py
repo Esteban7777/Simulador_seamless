918c09cd252f8c5d6f590ed1b3e1ba914661632a
Elian Joel Ruiz Callejas <elyan0811@gmail.com>
2023-05-12 12:00:50 -0500

    diseño

--- Simulador_seamless.py
+++ Simulador_seamless.py
@@ -2,7 +2,7 @@
 """
 Created on Sun May  7 09:36:08 2023
 
-@author: jelondos
+@author: jelondos, ejruizca
 """
 
 
@@ -11,13 +11,14 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import pandas as pd
-import numpy as np
+import numpy as np  
+from bs4 import BeautifulSoup
 from sklearn.model_selection import train_test_split
 import xgboost as xgb
 
 data = pd.read_csv('data.csv')
 
-X = data[['LARGO DE CUERPO', 'AREA','PESO PRENDA','DE_MODELO']]
+X = data[['LARGO DE CUERPO', 'AREA','PESO PRENDA']]
 y = data['Teorica']
 X = pd.get_dummies(X)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
@@ -25,84 +26,90 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_
 model = xgb.XGBRegressor()
 model.fit(X_train, y_train)
 
-app = dash.Dash('mi_app')
+external_stylesheets = ['./assets/style.css']
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-
-## Estas es la parte del front
 app.layout = html.Div(
-    style={'backgroundColor': '#f7f7f7', 'padding': '20px'},
+    className="contenedor",
     children=[
-        html.H1(
-            children='Simulador Seamless',
-            style={'textAlign': 'center', 'color': '#333333'}
-        ),
         html.Div(
-            children='Predicción de teórica basada en peso prenda, largo cuerpo y área.',
-            style={'textAlign': 'center', 'color': '#555555', 'marginBottom': '20px'}
-        ),
-
-        html.Div(
-            style={'display': 'flex', 'justifyContent': 'center', 'alignItems': 'center'},
             children=[
+                html.H2("simulador seamleass"),
+                html.H5("Predicción de teórica basada en peso prenda, largo cuerpo y área."),
+            ]
+        ),
+        html.Div(
+            className="contenedorDivsInputs",
+            children=[
+                html.Div( 
+                className="contenedorInputs",
+                children=[
+                    html.Div(
+                        className="contenedorInput",
+                        children=[
+                            html.Label("largo cuerpo:"),
+                            dcc.Input(type="number", id='input-largo', value=0),
+                        ]
+                    ),
+                    html.Div(
+                        className="contenedorInput",
+                        children=[
+                            html.Label("área:"),
+                            dcc.Input(type="number", id='input-area',  value=0),
+                        ]
+                    ), 
+                ]),
+               
                 html.Div(
-                    style={'marginRight': '20px'},
+                className='contenedorInput',
                     children=[
-                        html.Label('Largo cuerpo'),
-                        dcc.Input(id='input-largo', type='number', value=0),
-                    ]
-                ),
-                html.Div(
-                    style={'marginRight': '20px'},
-                    children=[
-                        html.Label('Área'),
-                        dcc.Input(id='input-area', type='number', value=0),
-                    ]
-                ),
-                html.Div(
-                    style={'marginRight': '20px'},
-                    children=[
-                        html.Label('Máquina'),
-                        dcc.Dropdown(
+                        html.Label('maquina:'),
+                        html.Select(
                             id='input-maquina',
-                            options=[
-                                {'label': 'SM8 TOP-2S', 'value': 'SM8 TOP-2S'},
-                                {'label': 'SM8 TOP', 'value': 'SM8 TOP'},
-                                {'label': 'TOP2 V', 'value': 'TOP2 V'},
-                                {'label': 'SM8 TR1', 'value': 'SM8 TR1'}
-                            ],
-                            value='SM8 TOP-2S',   # Valor predeterminado
-                            style={'width': '100%'}  # Ancho del dropdown ajustable
+                            children=[
+                                html.Option('Seleccione una opción', **{'data-value': 'SM8 TOP-2S'}),
+                                html.Option('SM8 TOP-2S', **{'data-value': 'SM8 TOP-2S'}),
+                                html.Option('SM8 TOP', **{'data-value': 'SM8 TOP'}),
+                                html.Option('SM8 TR1', **{'data-value': 'SM8 TR1'}),
+                                html.Option('TOP2 V', **{'data-value': 'TOP2 V'})
+                            ]
                         )
                     ]
+                )
+            ]
+        ),
+        html.Div(
+            children=[
+                html.Div(className="contendorButton", children=[html.Button("predecir", id='button')])
+            ]
+        ),
+        html.Div(
+            children=[
+                html.Div(
+                    className='contendorTeorica',
+                    children=[
+                        html.P('predicción de teórica: '),
+                        html.Span(id='output-teorica')
+                    ]
                 ),
-            ]
-        ),
-        
-        html.Div(
-            style={'display': 'flex', 'justifyContent': 'center', 'marginTop': '20px'},
-            children=[
-                html.Button('Predecir', id='button', style={'backgroundColor': '#4CAF50', 'color': 'white'}),
-            ]
-        ),
-
-        html.Div(
-            style={'textAlign': 'center', 'marginTop': '20px'},
-            children=[
-                html.Label('Predicción de teórica:', style={'marginRight': '10px'}),
-                html.Div(id='output-teorica', style={'display': 'inline-block', 'fontWeight': 'bold'}),
-            ]
-        ),
+                html.Div(
+                    className='contendorTeorica',
+                    children=[
+                        html.P('Peso: '),
+                        html.Span(id='output-peso')
+                    ]
+                ),
+        ]),
     ]
 )
 
-
 @app.callback(
+    dash.dependencies.Output('output-peso', 'children'),
     dash.dependencies.Output('output-teorica', 'children'),
     [dash.dependencies.Input('button', 'n_clicks')],
     [dash.dependencies.State('input-largo', 'value'),
      dash.dependencies.State('input-area', 'value'),
-     dash.dependencies.State('input-maquina', 'value')])
-
+     dash.dependencies.State('input-maquina', 'data-value')])
 def update_output(n_clicks, input_largo, input_area, input_maquina):
     peso_calculado = 1.40 * input_largo  # Calcula el peso basado en el largo del cuerpo
     input_data = np.array([[peso_calculado, input_largo, input_area]])
@@ -121,8 +128,7 @@ def update_output(n_clicks, input_largo, input_area, input_maquina):
     input_data_encoded = temp_df_encoded.reindex(columns=feature_names, fill_value=0)
     # Realizar la predicción
     prediction = model.predict(input_data_encoded)
-    return prediction[0]
-
+    return peso_calculado, prediction[0]
 
 if __name__ == '__main__':
-    app.run_server()
+    app.run_server(debug=True)
